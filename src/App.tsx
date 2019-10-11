a49422f01ab194c50170e18b5c21b34d98c6eef6
Slava Belaev <slav@belaev.pro>
2019-10-11 14:55:11 +0300

    feat(App): add ErrorBoundary for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import {withErrorBoundary} from "./components/ErrorBoundary";
 
 import {NotFoundPage} from "./pages/NotFoundPage";
 import {HomePage, HOME_PAGE_ROUTE_PATH} from "./pages/HomePage";
@@ -10,27 +11,28 @@ import {SearchPage, SEARCH_PAGE_ROUTE_PATH} from "./pages/SearchPage";
 import {SubscribePage, SUBSCRIBE_PAGE_ROUTE_PATH} from "./pages/SubscribePage";
 import {UnsubscribePage, UNSUBSCRIBE_PAGE_ROUTE_PATH} from "./pages/UnsubscribePage";
 import {UseTermsPage, USE_TERMS_PAGE_ROUTE_PATH} from "./pages/UseTermsPage";
-import {SignInPage, SIGN_IN_PAGE_ROUTE_PATH} from "./pages/SignInPage";
-import {SignUpPage, SIGN_UP_PAGE_ROUTE_PATH} from "./pages/SignUpPage";
-import {RecoveryPage, RECOVERY_PAGE_ROUTE_PATH} from "./pages/RecoveryPage";
+import {SIGN_IN_PAGE_ROUTE_PATH} from "./pages/SignInPage";
+import {SIGN_UP_PAGE_ROUTE_PATH} from "./pages/SignUpPage";
+import {RECOVERY_PAGE_ROUTE_PATH} from "./pages/RecoveryPage";
 import SwipeablePage from "./pages/SwipeablePage/SwipeablePage";
 
+const SwipeablePageWithErrorBoundary = withErrorBoundary(SwipeablePage);
 
 export default function() {
     return (
         <Router>
             <Layout>
                 <Switch>
-                    <Route path={HOME_PAGE_ROUTE_PATH} exact={true} component={HomePage} />
-                    <Route path={DETAILS_PAGE_ROUTE_PATH} component={DetailsPage} />
-                    <Route path={FAVORITE_PAGE_ROUTE_PATH} component={FavoritesPage} />
-                    <Route path={SEARCH_PAGE_ROUTE_PATH} component={SearchPage} />
-                    <Route path={SUBSCRIBE_PAGE_ROUTE_PATH} component={SubscribePage} />
-                    <Route path={UNSUBSCRIBE_PAGE_ROUTE_PATH} component={UnsubscribePage} />
-                    <Route path={USE_TERMS_PAGE_ROUTE_PATH} component={UseTermsPage} />
-                    <Route path={SIGN_IN_PAGE_ROUTE_PATH} component={SwipeablePage} />
-                    <Route path={SIGN_UP_PAGE_ROUTE_PATH} component={SwipeablePage} />
-                    <Route path={RECOVERY_PAGE_ROUTE_PATH} component={SwipeablePage} />
+                    <Route path={HOME_PAGE_ROUTE_PATH} exact={true} component={withErrorBoundary(HomePage)} />
+                    <Route path={DETAILS_PAGE_ROUTE_PATH} component={withErrorBoundary(DetailsPage)} />
+                    <Route path={FAVORITE_PAGE_ROUTE_PATH} component={withErrorBoundary(FavoritesPage)} />
+                    <Route path={SEARCH_PAGE_ROUTE_PATH} component={withErrorBoundary(SearchPage)} />
+                    <Route path={SUBSCRIBE_PAGE_ROUTE_PATH} component={withErrorBoundary(SubscribePage)} />
+                    <Route path={UNSUBSCRIBE_PAGE_ROUTE_PATH} component={withErrorBoundary(UnsubscribePage)} />
+                    <Route path={USE_TERMS_PAGE_ROUTE_PATH} component={withErrorBoundary(UseTermsPage)} />
+                    <Route path={SIGN_IN_PAGE_ROUTE_PATH} component={SwipeablePageWithErrorBoundary} />
+                    <Route path={SIGN_UP_PAGE_ROUTE_PATH} component={SwipeablePageWithErrorBoundary} />
+                    <Route path={RECOVERY_PAGE_ROUTE_PATH} component={SwipeablePageWithErrorBoundary} />
                     <Route component={NotFoundPage} />
                 </Switch>
             </Layout>
---
+++ src/components/ErrorBoundary/index.ts
@@ -0,0 +1,8 @@
+import { ErrorBoundary, withErrorBoundary } from "./ErrorBoundary";
+
+export {
+    ErrorBoundary,
+    withErrorBoundary
+}
+
+export default ErrorBoundary;
